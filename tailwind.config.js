c7e3a37373095ac27eaba5506a660e28929af7ca
Admin PMR <pmrsmanel@gmail.com>
2026-02-17 14:41:40 +0800

    Update V3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        red: {
-          800: '#991b1b',
-          900: '#7f1d1d',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
